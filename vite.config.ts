f8d78fa2d9a80fa5ca9795e2c4fbc2f948400cd9
Nhat Tan <66843927+lonhattan007@users.noreply.github.com>
2024-03-03 18:23:11 +0700

    Added dependencies and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), splitVendorChunkPlugin()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve('./assets'),
+    },
+  },
+});
