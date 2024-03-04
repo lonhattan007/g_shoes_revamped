a0f5cbeffcfea8bba0267b68e18ae0e757524899
Nhat Tan <66843927+lonhattan007@users.noreply.github.com>
2024-03-05 04:50:37 +0700

    Updated section card layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div
       id='app'
-      className='justify-items-center w-full h-full bg-white before:bg-yellow before:content-[""]
+      className='justify-items-center w-full h-full bg-white 
+			text-black
+			before:bg-yellow before:content-[""]
 			before:block before:fixed 
 			before:top-1/2 before:left-1/2 
 			before:w-[300%] before:h-full 
@@ -15,13 +17,16 @@ function App() {
     >
       {/*
 			before:-z-[1]
-			*/}
+		*/}
       <div
         id='app-content'
-        className='flex relative flex-col md:flex-row flex-wrap justify-between items-center py-0 my-0 mx-auto h-[100vh] px-[20px] max-w-[760px]'
+        className='flex relative flex-col md:flex-row flex-wrap 
+				items-center 
+				py-0 my-0 mx-auto h-[100vh] px-[20px] max-w-[760px]'
       >
-        <Section />
-        <Section />
+        {/* justify-between */}
+        <Section title='Our Products' />
+        <Section title='Your cart' />
       </div>
     </div>
   );
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,10 +1,31 @@
+import { ReactNode } from 'react';
 import './Section.css';
 
-function Section() {
+function Section(props: { title: string; children: ReactNode }) {
   return (
-    <div className='overflow-hidden flex-1 shrink-0 relative py-0 bg-white section-card w-[360px] h-[600px] rounded-[30px] px-[28px] mb-[20px] box-border'>
-      <div className='section-content before:content-[""] before:block before:absolute before:w-[300px] before:h-[300px] before:rounded-[100%] before:bg-yellow before:-top-[20%] before:-left-[50%] before:z-0'>
-        Card
+    <div
+      className='section-card 
+			overflow-hidden relative flex-col
+			py-0 px-[28px] mb-[20px] 
+			w-[360px] h-[600px] rounded-[30px] 
+			bg-white box-border
+			z-0
+			'
+    >
+      <div
+        className='section-content flex-col
+				before:content-[""] before:block 
+				before:w-[300px] before:h-[300px] before:rounded-[100%] 
+				before:bg-yellow 
+				before:absolute before:-top-[20%] before:-left-[50%] 
+				before:z-[-1]
+				'
+      >
+        <div className='mx-0 my-[12px] relative z-10'>
+          <img src='assets/images/nike.png' alt='Nike Logo' className='w-[50px]' />
+        </div>
+        <div className='text-[24px] font-bold text-left z-20'>{props.title}</div>
+        {props.children}
       </div>
     </div>
   );
