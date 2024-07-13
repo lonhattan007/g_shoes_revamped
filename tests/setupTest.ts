c761af4ed6339a6dd53701117356e9acfa8b03d9
Nhat Tan <66843927+lonhattan007@users.noreply.github.com>
2024-07-13 19:07:44 +0700

    Update concurrent tests setup

--- tests/setupTest.ts
+++ tests/setupTest.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest';
+import { describe, it, expect, test } from 'vitest';
 
 test('True to be true', () => {
   expect(true).toBe(true);
@@ -6,4 +6,13 @@ test('True to be true', () => {
 
 test('False to be false', () => {
   expect(false).toBe(false);
+});
+
+describe.concurrent('Concurrent suite', () => {
+  it('True to be true', async ({ expect }) => {
+    expect(true).toBe(true);
+  });
+  it('False to be false', async ({ expect }) => {
+    expect(false).toBe(false);
+  });
 });
